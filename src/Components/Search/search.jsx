7ce49e516d891b0727bdeb704ba4bae779483a09
Deividy <deividyosantos27@gmail.com>
2022-09-08 12:31:46 -0400

    use form to search

--- src/Components/Search/search.jsx
+++ src/Components/Search/search.jsx
@@ -8,7 +8,9 @@ const Search = () => {
     const navigate = useNavigate();
     const [search, setSearch] = useState('');
 
-    const seek = () => {
+    const seek = (e) => {
+        e.preventDefault();
+
         fetch('https://pokeapi.co/api/v2/pokemon/' + search.toLowerCase() )
         .then( (response) => response.json() )
         .then( (json) => {
@@ -17,10 +19,10 @@ const Search = () => {
     };
 
     return (
-        <div className="search">
+        <form onSubmit={(e) => seek(e)} className="search">
             <input className="searchInput" placeholder='Pesquisar' type="text" name="search" id="search" onChange={(e) => setSearch(e.target.value.toLowerCase())}/>
-            <BiSearch className="BiSearch" onClick={() => seek() }/>
-        </div>
+            <BiSearch className="BiSearch" onClick={(e) => seek(e)}/>
+        </form>
     );
 };
 
